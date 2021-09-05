5d1de828a20c5f5840fbaceff39957d160437e10
Veikko Aho <vidfinnx@yahoo.com>
2021-09-04 18:01:55 -0700

    snake game

--- README.md
+++ README.md
@@ -1,70 +1,22 @@
-# Getting Started with Create React App
+# Phaser-Webpack
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Simple boilerplate project that combines [Phaser](http://phaser.io) with [Webpack](http://webpack.github.io).
 
-## Available Scripts
+## Quick start
 
-In the project directory, you can run:
+```
+$ npm install      # install dependencies
+$ npm start        # run local server
+$ npm run build    # prepare your game for deployment (available on the public folder)
+```
 
-### `npm start`
+## Notes
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+This is supposed to be a completely barebones project.
+There's no extra candy. No ES6, uglyfing your code, compressing your images. Nada.
+By providing only the minimum setup you will be able to customize it however you like,
+and will make learning the needed steps to integrate webpack and phaser really easy.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+## How it works
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+The only requirement that you need to be aware when using Phaser with Webpack is that Phaser relies on some globally available variables. That can be accomplished in a controlled way by using the `expose-loader` package. We must import the required modules through this loader before importing Phaser. And that's it. No extra configuration is needed.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,355 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+ * Import Phaser dependencies using `expose-loader`.
+ * This makes then available globally and it's something required by Phaser.
+ * The order matters since Phaser needs them available before it is imported.
+ */
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import PIXI from 'expose-loader?PIXI!phaser-ce/build/custom/pixi.js';
+import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2.js';
+import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/**
+ * Create a new Phaser game instance.
+ * And render a single sprite so we make sure it works.
+ */
+
+var gridSize = 17;
+var player;
+var currentSpeed = 0;
+var speed = 10;
+var borderOffset = 1;
+var borderTop = 50;
+var borderSide = 50;
+
+
+var game = new Phaser.Game((gridSize * gridSize) + (borderSide * 2), (gridSize * gridSize) + borderTop, Phaser.AUTO,);
+
+
+var direction = 'right';
+
+var upKey;
+var downKey;
+var leftKey;
+var rightKey;
+var lastMoved = 'right';
+
+var tail = [];
+var initialTailLength = 1;
+var food_item;
+
+var food_overlap;
+var tail_overlap;
+
+var gridWidth = (gridSize * gridSize) + (borderSide * 2);
+var gridHeight = (gridSize * gridSize) + borderTop;
+
+var gamestart;
+var highscore = 0;
+var score = 0;
+var score_amount;
+var score_text;
+var width_offset = 15;
+
+var textColour = '#FFFFFF';
+
+var Game = {
+	preload: function () {
+		game.load.image('player', './assets/images/green.png');
+		game.load.image('food', './assets/images/food.png');
+		game.load.image('head', './assets/images/head.png');
+
+		game.load.audio('eat', ['./assets/sound/eat.mp3', './assets/sound/eat.ogg']);
+	},
+
+	create: function () {
+		
+		this.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
+		// Reset Variables since when you restart an instance this stuff doesn't get reset
+		tail = [];
+		score = 0;
+
+		var graphics = game.add.graphics(0, 0);
+
+		var x = Math.ceil(gridSize / 2) * gridSize + (borderSide);
+		var y = Math.ceil(gridSize / 2) * gridSize;
+
+		y += (borderTop)
+
+		for (var i = 1; i < initialTailLength + 1; i++) {
+			tail[i - 1] = game.add.sprite(x - (i * gridSize) + (borderSide + 2), y + 1, 'head');
+			
+		}
+
+		game.stage.backgroundColor = '#484b5e';
+
+		// Setup Controls
+		upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+		downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+		leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+		rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+		// Build the grid
+		// Columns
+		for (var i = 0; i < gridSize; i++) {
+			// Rows
+			for (var n = 0; n < gridSize; n++) {
+				graphics.moveTo(borderSide, borderTop);
+				graphics.lineStyle(1, 0x0f380f, 1);
+				graphics.beginFill(0x9bbc0f);
+				graphics.drawRect((gridSize * i) + (borderSide), (gridSize * n) + borderTop, gridSize, gridSize);
+				graphics.endFill();
+			}
+		}
+
+		// Draw score title
+		score_text = this.game.add.text((game.world.width / 2) - width_offset, borderTop / 2, "Score: ", { font: "2rem Wendy One", fill: textColour });
+		score_text.anchor.setTo(0.5);
+
+		score_amount = this.game.add.text((game.world.width / 2) + (score_text.width / 2) + width_offset, borderTop / 2, score, { font: "2rem Wendy One", fill: textColour });
+		score_amount.anchor.setTo(0.5);
+
+		
+
+		game.time.events.add(Phaser.Timer.SECOND * 0,startGame, this)
+
+		function startGame(){
+		if (speed == 10 && gamestart == "intro"){
+			this.getready.destroy();
+			gamestart = "running"
+				
+		}else{
+		this.getready = this.game.add.text(400, 300, 'Get Ready!', { font: "1.84rem Wendy One", fill: textColour });
+		this.getready.anchor.setTo(1.85, 4)	
+		gamestart = 'intro'
+		    
+			game.time.events.add(Phaser.Timer.SECOND * 1.5,startGame, this)
+			
+		}
+	}
+			
+	
+		direction = 'right';
+		lastMoved = 'left';
+
+		// Generate the food
+		this._generateFood();
+		
+		
+	},	
+
+	update: function () {
+		// Set Direction
+		this._set_direction();
+
+		// Check not tail collision
+		this._check_tail_collision();
+
+		// Check if food
+		this._check_food_eat();
+
+		// Movement
+		this._handle_movement();
+
+		// Check death
+		this._wrap_world();
+
+		
+	},
+
+	_wrap_world: function () {
+		if (tail[0].x > game.world.width - (borderSide)) {
+			tail[0].x = (borderSide + 1);
+		}
+
+		if (tail[0].x < borderSide) {
+			tail[0].x = (game.world.width - borderSide) - (gridSize - 1);
+		}
+
+		if (tail[0].y > game.world.height) {
+			tail[0].y = (borderTop + 1);
+		}
+
+		if (tail[0].y < borderTop) {
+			tail[0].y = ((game.world.height - gridSize) + 1);
+		}
+	},
+
+	_set_direction: function () {
+		if (leftKey.isDown && lastMoved != "right") direction = "left";
+		else if (upKey.isDown && lastMoved != "down") direction = "up";
+		else if (rightKey.isDown && lastMoved != "left") direction = "right";
+		else if (downKey.isDown && lastMoved != "up") direction = "down";
+	},
+
+	_handle_movement: function () {
+		if (currentSpeed == speed) {
+			switch (direction) {
+				case 'up':
+					this._movePlayer(tail[0].x, tail[0].y - gridSize);
+					lastMoved = direction;
+					break;
+
+				case 'down':
+					this._movePlayer(tail[0].x, tail[0].y + gridSize);
+					lastMoved = direction;
+					break;
+
+				case 'left':
+					this._movePlayer(tail[0].x - gridSize, tail[0].y);
+					lastMoved = direction;
+					break;
+
+				case 'right':
+					this._movePlayer(tail[0].x + gridSize, tail[0].y);
+					lastMoved = direction;
+					break;
+			}
+
+			currentSpeed = 0;
+		}
+
+		currentSpeed++;
+	},
+
+	_movePlayer: function (x, y) {
+		var oldPosition = [];
+		for (var i = 0; i < tail.length; i++) {
+			oldPosition.push({ 'x': tail[i].x, 'y': tail[i].y });
+		}
+
+		this._check_tail_collision();
+
+		// Set a array for all old tail data to use later
+		for (var i = 0; i < tail.length; i++) {
+			if (i == 0) {
+				tail[i].x = x;
+				tail[i].y = y;
+			}
+			else {
+				var newI = i - 1;
+				tail[i].x = oldPosition[i - 1].x;
+				tail[i].y = oldPosition[i - 1].y;
+			}
+		}
+	},
+
+	_generateFood: function () {
+		// Generate random x and y
+		var x = this._generateX();
+		var y = this._generateY();
+
+		// Loop through the tail and check that our spawn position doesn't match the position of the snake.
+		// If it does we want to regenerate until it doesn't.
+		for (var i = 0; i < tail.length; i++) {
+			var match = false;
+			while (match == false) {
+				var positionX = (x * gridSize) + (borderOffset + borderSide);
+				var positionY = (y * gridSize) + (borderOffset + borderTop);
+
+				if (tail[i].x == positionX && tail[i].y == positionY) {
+					x = this._generateX();
+					y = this._generateY();
+				}
+				else {
+					match = true;
+				}
+			}
+		}
+
+		food_item = game.add.sprite((x * gridSize) + (borderOffset + borderSide), (y * gridSize) + (borderOffset + borderTop), 'food');
+	},
+
+	_generateX: function () {
+		var x = Math.floor(Math.random() * (gridSize));
+
+		return x;
+	},
+
+	_generateY: function () {
+		var y = Math.floor(Math.random() * (gridSize));
+
+		return y;
+	},
+
+	_trigger_death: function () {
+		this.yourhighscore = this.add.text(400, 300, 'Your HighScore:' +`${highscore}`, { font: "1.84rem Wendy One", fill: textColour });
+		this.yourhighscore.anchor.setTo(1.35, 6.2)
+		this.tryagaintext = this.add.text(400, 300, 'Click Here Try Again', { font: "1.84rem Wendy One", fill: textColour });
+		this.tryagaintext.anchor.setTo(1.21, 4.2)
+		this.tryagaintext.inputEnabled = true;
+		this.tryagaintext.events.onInputDown.add(this.restartGame,this);
+		this.gameovertext = this.add.text(400, 300, 'Game Over', { font: "2rem Wendy One", fill: textColour });
+		this.gameovertext.anchor.setTo(1.75, 3)
+		this.gameovertext.inputEnabled = true;
+		this.gameovertext.events.onInputDown.add(this.restartGame,this);
+		this.gamestart = "gameover"
+		game.paused = true;
+	},
+
+	restartGame: function () {
+		speed = 10;
+		game.paused = false;
+		game.state.restart();
+	},
+
+	
+
+	_check_tail_collision: function () {
+		// Check if the head of the snake overlaps with any part of the snake.
+		for (var i = 1; i < tail.length; i++) {
+			if (tail[0].x == tail[i].x && tail[0].y == tail[i].y) {
+
+				// If so, go to game over screen.
+				this._trigger_death();
+			}
+		}
+	},
+
+	_check_food_eat: function () {
+		// Check if theres some food
+		if (tail[0].x == food_item.x && tail[0].y == food_item.y) {
+			this._eat_food();
+			
+			
+		}
+	},
+
+	_eat_food: function (first_tail) {
+		food_item.destroy();
+
+		var nextX = -100;
+		var nextY = -100;
+
+		var tail_item = game.add.sprite(nextX, nextX, 'player');
+
+		tail.push(tail_item);
+		
+
+		score++;
+		if (speed > 5)
+		speed--;
+		if (score > highscore) {
+			highscore++
+			localStorage.setItem('snakeHighScore', highscore)
+			
+		}
+
+
+
+		var scoreTween = game.add.tween(score_amount.scale).to({ x: 1.2, y: 1.2 }, 150, "Linear", true);
+		score_amount.text = score;
+
+		scoreTween.onComplete.add(this._scaleDownText, true);
+
+		game.sound.play('eat');
+
+		this._generateFood();
+	},
+
+	_scaleDownText: function () {
+		game.add.tween(score_amount.scale).to({ x: 1, y: 1 }, 150, "Linear", true);
+	}
+}
+
+game.state.add('Game', Game);
+game.state.start('Game');
